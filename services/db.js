430c74754cd98ba5baca18c80eaa2212d43546ca
Axile24 <amokrane.n@icloud.com>
2025-09-17 22:53:44 +0200

    Clean up services and responses DynamoDb funkar

--- services/db.js
+++ services/db.js
@@ -1,11 +1,20 @@
+/**
+ * Database Service
+ * This file sets up the connection to AWS DynamoDB
+ * Perfect for beginners to understand how database connections work
+ */
+
 const AWS = require('aws-sdk');
 
+// Configure AWS region
 AWS.config.update({
   region: process.env.AWS_REGION || 'eu-north-1'
 });
 
+// Create DynamoDB client
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
+// Export the client so other files can use it
 module.exports = {
-  db: dynamodb // Exportera klienten för att kunna återanvändas
+  db: dynamodb
 };
